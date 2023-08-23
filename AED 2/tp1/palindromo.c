e29b074cf88f3e1370eebeb9e26ebccfd95620fa
Luis Augusto Lima de Oliveira <66066037+LuisAugusto0@users.noreply.github.com>
2023-08-23 14:41:44 -0300

    .

---
+++ AED 2/tp1/palindromo.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+bool ehPalindromo(char frase[], int tam){
+	bool palindromo=true;
+	for(int pos=0; pos<=(tam-1)/2; pos++){
+		//para testar se possui caractere especial e tratar se tiver
+		if((int)frase[pos] == -61){
+			if(frase[tam-pos-1] != frase[pos+1]){
+				palindromo=false;
+				pos=tam;
+			}
+			pos++;
+		} else if(frase[pos] != frase[tam-pos-1]){
+			palindromo = false;
+			pos = tam;
+		}
+	}
+	return palindromo;
+}
+
+int main (void) {
+	//declaração de variáveis
+	int tam=0;
+        char frase[150];
+
+	//leitura de dados e verificar tam do array	
+	fgets(frase, 150, stdin);
+	tam = strlen(frase)-1;
+	//executar enquanto a variável frase não tiver um tamanho de 3 caracteres e que seja diferente de FIM.
+	while(tam != 3 || frase[0]!='F' || frase[1]!='I' || frase[2]!='M'){
+		bool palindromo = ehPalindromo(frase, tam);
+		if(palindromo){
+			puts("SIM");
+		} else {
+			puts("NAO");
+		}
+
+		//leitura de dados e verificar tam do array 
+		fgets(frase, 150, stdin); 
+	        //while(frase[tam] != '\n') tam++;
+		tam = strlen(frase)-1;
+	}
+	return 0;
+}
+
---
+++ AED 2/tp1/teste.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){
+	puts("olá");
+	puts("Tudo bem?");
+	return 0;
+}
