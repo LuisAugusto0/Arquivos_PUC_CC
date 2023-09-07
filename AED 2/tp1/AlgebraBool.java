1a56e62637ad06873de3cc4b27df19c3f3dce160
Luis Augusto Lima de Oliveira <laloaugusto78@gmail.com>
2023-09-07 20:15:41 -0300

    nw

--- AED 2/tp1/AlgebraBool.java
+++ AED 2/tp1/AlgebraBool.java
@@ -1,11 +1,26 @@
 class AlgebraBool{
-	static int algebra(String params){
-		boolean[] valor = new boolean[params.charAt(0)];
+    static boolean algebraRecursiva(String params){
+
+    }
+    static int algebra(String params){
+		int qtdeElementos = params.charAt(0);
+        int[] valor = new int[params.charAt(0)];
+
 		int c=2;
-		for(int i=0; i<(int)params.charAt(0); i++, c+=2){
-			valor[i] = params.charAt(c);
+		for(int i=0; i<qtdeElementos; i++, c+=2){
+			valor[i] = params.charAt(i);
 		}
-		for(int i
+        
+        if(qtdeElementos == 3){
+            for(int i=params.length(); i>c; i--){
+                if(params.charAt(i) == ')'){
+                    int aux;
+                    while(params.charAt(i) != '('){
+                        if()
+                    }
+                } 
+            }
+        }
 	}
 	public static void main (String[] args){
 	        String params;
