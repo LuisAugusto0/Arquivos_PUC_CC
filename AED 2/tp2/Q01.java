112508ccc87ebc8550090207b4a8f89a94531a1d
LuisAugusto0 <laloaugusto78@gmail.com>
2023-09-28 10:35:18 -0300

    klasdgsg

---
+++ AED 2/tp2/Q01.java
@@ -0,0 +1,178 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+class Jogador{
+    private int id;
+    private String nome;
+    private int altura;
+    private int peso;
+    private String universidade;
+    private int anoNascimento;
+    private String cidadeNascimento;
+    private String estadoNascimento;
+    
+    Jogador(){
+        setAll(0, "", 0, 0, "", 0, "", "");
+    }
+
+    Jogador(int id){
+        ler(id);
+    }
+    Jogador(int id, String nome, int altura, int peso, String universidade, int anoNascimento, String cidadeNascimento, String estadoNascimento){
+        setAll(id, nome, altura, peso, universidade, anoNascimento, cidadeNascimento, estadoNascimento);
+    }
+
+    //metodos set
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setNome(String nome){
+        if(nome == null){
+             this.nome = "nao informado";
+         } else {
+             this.nome = nome;
+         }
+    }
+    public void setAltura(int altura){
+        this.altura = altura;
+    }
+    public void setPeso(int peso){
+        this.peso = peso;
+    }
+    public void setUniversidade(String universidade){
+       if(universidade == ""){
+            this.universidade = "nao informado";
+        } else {
+            this.universidade = universidade;
+        }
+    }
+    
+    public void setAnoNascimento(int ano){
+        this.anoNascimento = ano;
+    }
+    public void setCidadeNascimento(String cidade){
+        if(cidade == ""){
+            this.cidadeNascimento = "nao informado";
+        } else {
+            this.cidadeNascimento = cidade;
+        }
+    }
+    public void setEstadoNascimento(String estado){
+        if(estado == ""){
+            this.estadoNascimento = "nao informado";
+        } else {
+            this.estadoNascimento = estado;
+        }
+    }
+
+    public void setAll(int id, String nome, int altura, int peso, String universidade, int anoNascimento, String cidadeNascimento, String estadoNascimento){
+        setId(id);
+        setNome(nome);
+        setAltura(altura);
+        setPeso(peso);
+        setUniversidade(universidade);
+        setAnoNascimento(anoNascimento);
+        setCidadeNascimento(cidadeNascimento);
+        setEstadoNascimento(estadoNascimento);
+
+    }
+
+    //metodos get
+    public int getId(){
+        return id;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public int getAltura(){
+        return altura;
+    }
+    public int getPeso(){
+        return peso;
+    }
+    public String getUniversidade(){
+        return universidade;
+    }
+    public int getAnoNascimento(){
+        return anoNascimento;
+    }
+    public String getCidadeNascimento(){
+        return cidadeNascimento;
+    }
+    public String getEstadoNascimento(){
+        return estadoNascimento;
+    }
+
+    //split para a leitura
+    private String[] split(String linha){
+        String[] coluna = {"", "0", "0", "", "0", "", "", ""};
+        int pos = 0;
+        if(linha != null){
+            for(int i=0; i<linha.length(); i++){
+                String aux = "";
+ 
+                while(i<linha.length() && linha.charAt(i) != ','){
+                    aux += linha.charAt(i++);
+                }
+ 
+                coluna[pos] = aux;
+                pos++;
+            }
+        }
+        return coluna;
+    }
+    //metodo de leitura
+    public void ler(int id){
+        try{
+            File file = new File("/tmp/players.csv");
+            BufferedReader arq = new BufferedReader( new FileReader(file) );
+            arq.readLine(); //pular primeira linha das informacoes das colunas
+            String[] splitted = split(arq.readLine());
+            while( Integer.valueOf(splitted[0]) != id )
+                splitted = split(arq.readLine());
+            
+            String nome = splitted[1];
+            int altura = Integer.valueOf(splitted[2]);
+            int peso = Integer.valueOf(splitted[3]);
+            String universidade = splitted[4];
+            int anoNascimento = Integer.valueOf(splitted[5]);
+            String cidadeNascimento = splitted[6];
+            String estadoNascimento = splitted[7];
+
+            setAll(id, nome, altura, peso, universidade, anoNascimento, cidadeNascimento, estadoNascimento); 
+        } catch(IOException e){
+            MyIO.println("Um erro ocorreu.");
+            e.printStackTrace();
+        } catch(NumberFormatException e){
+            MyIO.println("Id invalido");
+        }
+    }
+    
+    //metodo de escrita
+    public void imprimir(){
+        MyIO.println("["+ getId() + " ## " + getNome() + " ## " + getAltura() +  " ## " + getPeso() + " ## " + getAnoNascimento() + " ## " + getUniversidade() + " ## " + getCidadeNascimento() + " ## " + getEstadoNascimento() + "]");
+    }
+
+    //metodo de clone
+ 
+    public Jogador clone() {
+        return new Jogador(id, nome, altura, peso, universidade, anoNascimento, cidadeNascimento, estadoNascimento);
+    }
+}
+
+
+class Q01{
+    public static void main(String[] args){
+        Jogador escolhido = new Jogador();
+        String id;
+        id = MyIO.readLine();
+        while(!id.equals("FIM")){
+           escolhido.ler(Integer.valueOf(id));
+           escolhido.imprimir();
+           id = MyIO.readLine();
+        }
+    }
+}
---
+++ AED 2/tp2/q02.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+struct jogador{
+    int id;
+    char nome[50];
+    int peso;
+    char universidade[50];
+    int anoNascimento;
+    char cidadeNascimento[50];
+    char estadoNascimento[50];
+};
+typedef struct jogador jogador;
+
+void lerInt(int*);
+void lerString(char[]);
+
+int main()
+{
+    
+}
